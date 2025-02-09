4d9a12bee499e735b612817c332c19107342164f
Nicholas Boleky <bolekyn@gmail.com>
2025-02-08 18:42:23 -0800

    Initial Commit

---
+++ SpeechTrak/SpeechTrakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechTrakApp.swift
+//  SpeechTrak
+//
+//  Created by Nick on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SpeechTrakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpeechTrakTests/SpeechTrakTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechTrakTests.swift
+//  SpeechTrakTests
+//
+//  Created by Nick on 2/8/25.
+//
+
+import Testing
+@testable import SpeechTrak
+
+struct SpeechTrakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
